a1a6bae29b77a571baaeca5aa84acfe3f8c21beb
Nikita Borisov <velikiegeevcy@gmail.com>
2025-01-04 10:17:55 +0400

    Add ProductCopy.java

---
+++ src/ProductCopy.java
@@ -0,0 +1,12 @@
+public class ProductCopy {
+    // добавить модификаторы доступа
+    String article;
+    String productId;
+    String shopId;
+
+    public ProductCopy (String article, String productId, String shopId) {
+        this.article = article;
+        this.productId = productId;
+        this.shopId = shopId;
+    }
+}
