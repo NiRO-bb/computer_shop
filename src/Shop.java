3651eb40b9b60bc6e6315d0438a16f6409c8fd67
Nikita Borisov <velikiegeevcy@gmail.com>
2025-01-04 10:19:23 +0400

    Add Shop.java

---
+++ src/Shop.java
@@ -0,0 +1,14 @@
+public class Shop {
+    // добавить модификатор доступа
+    String id;
+    String city;
+    String street;
+    int building;
+
+    public Shop(String id, String city, String street, int building) {
+        this.id = id;
+        this.city = city;
+        this.street = street;
+        this.building = building;
+    }
+}
