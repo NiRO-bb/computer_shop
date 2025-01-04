be9c10ef221b05a5e79468b5c84de5938e32aa39
Nikita Borisov <velikiegeevcy@gmail.com>
2025-01-04 11:42:42 +0400

    Add SQL.java

---
+++ src/SQL.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+public class SQL {
+    private static String URL = "jdbc:mysql://localhost:3306/niro_bb";
+    private static String dbUsername = "niro_bb";
+    private static  String dbPassword = "2034";
+
+    private static Connection conn;
+    private static Statement statement;
+}
