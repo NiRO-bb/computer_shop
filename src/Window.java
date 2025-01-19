4608c6a3dea098f969349cb091aa67b56e46abb8
Nikita Borisov <velikiegeevcy@gmail.com>
2025-01-19 12:39:15 +0400

    Update Window.java

--- src/Window.java
+++ src/Window.java
@@ -1,6 +1,9 @@
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // класс описывает общую логику всех окон приложения
 public abstract class Window extends JDialog {
@@ -25,4 +28,35 @@ public abstract class Window extends JDialog {
 
     // создание содержимого окна
     protected abstract JPanel CreateContent();
+
+    // поиск - формирует список элементов, удовлетворяющих запросу
+    // затем требуется самостоятельно выполнить добавление элементов в панель
+    ArrayList<Object> search(JTextField searchField, ArrayList<Object> elements) {
+        Pattern pattern = Pattern.compile(searchField.getText().toLowerCase(), Pattern.CASE_INSENSITIVE);
+        Matcher matcher;
+
+        // формируется новый список
+        ArrayList<Object> searchResult = new ArrayList<>();
+        for (Object obj : elements) {
+            matcher = pattern.matcher((obj.toString()).toLowerCase());
+
+            if (matcher.find())
+                searchResult.add(obj);
+        }
+
+        // вернуть новый список
+        return searchResult;
+    }
+
+    // Добавить описание элемента
+    JPanel addDesc(Object element, int number) {
+        // панель надписи
+        JPanel labelPanel = new JPanel();
+        labelPanel.setLayout(new BoxLayout(labelPanel, 0));
+
+        labelPanel.add(new JLabel("%s. %s".formatted(number, element.toString())));
+        labelPanel.add(Box.createGlue());
+
+        return labelPanel;
+    }
 }
